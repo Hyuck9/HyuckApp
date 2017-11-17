281e211f79467e5965cbe49c8539654d7ce1cafa
Hyuck9 <lhg1304@gmail.com>
2017-11-17 10:57:28 +0900

    채팅방 목록 출력

---
+++ app/src/main/java/com/lhg1304/hyuckapp/page02/firemessenger/adapters/ChatListAdapter.java
@@ -0,0 +1,93 @@
+package com.lhg1304.hyuckapp.page02.firemessenger.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lhg1304.hyuckapp.R;
+import com.lhg1304.hyuckapp.page02.firemessenger.customviews.RoundedImageView;
+import com.lhg1304.hyuckapp.page02.firemessenger.models.Chat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by lhg1304 on 2017-11-15.
+ */
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatHolder> {
+
+    private ArrayList<Chat> mChatList;
+    private SimpleDateFormat sdf = new SimpleDateFormat("MM/dd\naa hh:mm");
+    public ChatListAdapter() {
+        mChatList = new ArrayList<>();
+    }
+
+    public void addItem(Chat chat) {
+        mChatList.add(chat);
+        notifyDataSetChanged();
+    }
+
+    public Chat getItem(int position) {
+        return this.mChatList.get(position);
+    }
+
+    @Override
+    public ChatHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.page_02_fragment_messenger_chat_item, parent, false);
+        return new ChatHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ChatHolder holder, int position) {
+
+        Chat item = getItem(position);
+
+        // chatThumbnailView
+
+        holder.lastMessageView.setText(item.getLastMessage().getMessageText());
+        holder.titleView.setText(item.getTitle());
+        holder.lastMessageDateView.setText(sdf.format(item.getCreateDate()));
+        if ( item.getTotalUnreadCount() > 0 ) {
+            holder.totalUnreadCountView.setText(String.valueOf(item.getTotalUnreadCount()));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+
+    public static class ChatHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.messenger_chat_item_thumb)
+        RoundedImageView chatThumbnailView;
+
+        @BindView(R.id.messenger_chat_item_title)
+        TextView titleView;
+
+        @BindView(R.id.messenger_chat_item_last_message)
+        TextView lastMessageView;
+
+        @BindView(R.id.messenger_chat_item_total_unread_count)
+        TextView totalUnreadCountView;
+
+        @BindView(R.id.messenger_chat_item_last_message_date)
+        TextView lastMessageDateView;
+
+        public ChatHolder(View v) {
+            super(v);
+
+            ButterKnife.bind(this, v);
+        }
+    }
+
+}
--- app/src/main/java/com/lhg1304/hyuckapp/page02/firemessenger/views/MessengerChatFragment.java
+++ app/src/main/java/com/lhg1304/hyuckapp/page02/firemessenger/views/MessengerChatFragment.java
@@ -2,21 +2,144 @@ package com.lhg1304.hyuckapp.page02.firemessenger.views;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lhg1304.hyuckapp.R;
+import com.lhg1304.hyuckapp.page02.firemessenger.adapters.ChatListAdapter;
+import com.lhg1304.hyuckapp.page02.firemessenger.models.Chat;
+import com.lhg1304.hyuckapp.page02.firemessenger.models.User;
+
+import java.util.Iterator;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MessengerChatFragment extends Fragment {
 
+    private FirebaseUser mFirebaseUser;
+
+    private FirebaseDatabase mFirebaseDatabase;
+
+    private DatabaseReference mChatDBRef;
+
+    private DatabaseReference mChatMemberDBRef;
+
+    @BindView(R.id.messenger_chat_recyclerview)
+    RecyclerView mChatRecyclerView;
+
+    private ChatListAdapter mChatListAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.page_02_fragment_messenger_chat, container, false);
+        View chatView = inflater.inflate(R.layout.page_02_fragment_messenger_chat, container, false);
+        ButterKnife.bind(this, chatView);
+
+
+        // 채팅방 리스너 부착
+        // user > {my_uid} > chats
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mChatDBRef = mFirebaseDatabase.getReference("users").child(mFirebaseUser.getUid()).child("chats");
+        mChatMemberDBRef = mFirebaseDatabase.getReference("chat_members");
+        mChatListAdapter = new ChatListAdapter();
+        mChatRecyclerView.setAdapter(mChatListAdapter);
+        mChatRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        addChatListener();
+
+        return chatView;
     }
+
+    private void addChatListener() {
+        mChatDBRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(final DataSnapshot chatDataSnapshot, String s) {
+
+                // 방에 대한 정보를 얻어오고
+                final Chat chatRoom = chatDataSnapshot.getValue(Chat.class);
+                mChatMemberDBRef.child(chatRoom.getChatId()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        long memberCount = dataSnapshot.getChildrenCount();
+                        Iterator<DataSnapshot> memberIterator = dataSnapshot.getChildren().iterator();
+                        StringBuffer memberStringBuffer = new StringBuffer();
+
+                        int loopCount = 1;
+                        while( memberIterator.hasNext() ) {
+                            User member = memberIterator.next().getValue(User.class);
+                            memberStringBuffer.append(member.getName());
+
+                            if ( loopCount < memberCount ) {
+                                memberStringBuffer.append(", ");
+                            }
+
+                            if ( loopCount == memberCount ) {
+                                String title = memberStringBuffer.toString();
+                                if ( chatRoom.getTitle() != null ) {
+                                    if ( !chatRoom.getTitle().equals(title) ) {
+                                        // users > {uid} > chats > {chat_id} > title
+                                        chatDataSnapshot.getRef().child("title").setValue(title);
+                                    }
+                                }
+                                // UI 갱신 시켜주는 메서드로 방의 정보를 전달
+                                drawUI( chatRoom );
+                            }
+                            loopCount++;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                // 기존의 방제목과 방 멤버의 이름들을 가져와서 타이틀화 시켰을 때 같지 않은 경우 방제목을 업데이트
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                // 변경된 방의 정보를 수신
+                // 내가 보낸 메시지가 아닌 경우와 마지막 메시지가 수정이 되었다면 -> 노티출력
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void drawUI(Chat chat) {
+        mChatListAdapter.addItem(chat);
+    }
+
 }
